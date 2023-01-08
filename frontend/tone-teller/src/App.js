b2487644809b831e42365c0f7ddcaf293a3495b9
Christina <christinaraganit@gmail.com>
2023-01-08 03:04:44 -0800

    push push

--- frontend/tone-teller/src/App.js
+++ frontend/tone-teller/src/App.js
@@ -1,30 +1,29 @@
 import React from "react";
-import Hero from "./components/Hero";
-import LandingContent from "./components/LandingContent";
 import "./App.css";
 
+import Home from "./components/Home";
 // import the landing page from the components folder
 import NavHeader from "./components/NavHeader";
 import Footer from "./components/Footer";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from './components/pages/About';
-import ChromeExtension from './components/pages/ChromeExtension';
-import FAQ from './components/pages/FAQ';
-import SignUp from './components/pages/SignUp';
+import About from "./components/pages/About";
+import ChromeExtension from "./components/pages/ChromeExtension";
+import FAQ from "./components/pages/FAQ";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
     <>
       <Router>
         <NavHeader />
-        <Hero></Hero>
-        <LandingContent></LandingContent>
+        <ChromeExtension></ChromeExtension>
         <Routes>
-          <Route path='/about' component={About} />
-          <Route path='/chrome' component={ChromeExtension} />
-          <Route path='/FAQ' component={FAQ} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/chrome" component={ChromeExtension} />
+          <Route path="/FAQ" component={FAQ} />
+          <Route path="/sign-up" component={SignUp} />
         </Routes>
         <Footer />
       </Router>
