cb1c9b6bd1dd8c043f81a7e3b6c97d03cb66509c
Aniket Mishra <ianiket23@gmail.com>
2023-01-08 06:00:01 -0700

    backend + frontend connected
    able to fetch the data

--- frontend/tone-teller/src/components/pages/ToneTeller.js
+++ frontend/tone-teller/src/components/pages/ToneTeller.js
@@ -1,7 +1,30 @@
-import React from 'react';
 import '../../App.css';
+import React, {useState} from "react";
+import axios from "axios";
 
 export default function ToneTeller() {
-  console.log("Tone Teller page");
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  const [data,setData]=useState(null)
+  function getData(val)
+  {
+    setData(val.target.value)
+  }
+  async function getDataBackend()
+  {
+    // axios call to backend async and await and print to console else print error
+    await axios.post("https://tone-teller-ezen7qibyq-nn.a.run.app/tonetelling", {
+      input : data
+    }).then(res => {
+      console.log(res.data)}
+    )
+    .catch(err => {
+      console.log(err)
+    })
+
+  }
+  return (
+    <div className="ToneTeller">
+      <input type="text" onChange={getData}/>
+      <button onClick={()=>getDataBackend()} >Analyze</button>
+    </div>
+  );
 }
