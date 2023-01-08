5df6e84e8ca9cbc08a91908ede72fd0339b199d3
goldentoaste <rayg2375@gmail.com>
2023-01-08 00:36:15 -0700

    api stuff

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,21 @@
 # py -m flask run
 
-from flask import Flask, render_template, url_for, flash, redirect, request
-from forms import TextForm
+from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
+
 import os
+from transformers import pipeline
+
+
+modelPath = "./model"
+emotion = pipeline("sentiment-analysis", model=modelPath, tokenizer=modelPath, top_k = None)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-@app.route("/")
-def home():
-    form = TextForm()
-    return render_template('form.html', title='Register', form = form)
+# @app.route("/")
+# def home():
+#     form = TextForm()
+#     return render_template('form.html', title='Register', form = form)
 
 
 @app.route('/submit/', methods=['POST'])
@@ -18,7 +23,18 @@ def submit():
     if request.method == 'POST':
         form_data = request.form
         return render_template('results.html', result = form_data['text'])
+    
+    
+@app.route("/tonetelling", methods=["POST"])
+def tonetelling():
+    
+    data = request.json
+    
+    if "input" in data:
+        return emotion(data["input"])
+    return jsonify(code = 400, message="bad input")
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
