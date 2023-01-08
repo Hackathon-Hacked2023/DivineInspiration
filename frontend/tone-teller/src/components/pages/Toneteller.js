495c2881dd80e6b42f3572594b9f5ec0781acb9f
Christina <christinaraganit@gmail.com>
2023-01-08 06:57:53 -0800

    push push

--- frontend/tone-teller/src/components/pages/Toneteller.js
+++ frontend/tone-teller/src/components/pages/Toneteller.js
@@ -1,7 +1,7 @@
-export default function Toneteller() {
-  return (
-    <div className="toneteller-app">
-      <div className=""></div>
-    </div>
-  );
+import React from 'react';
+import '../../App.css';
+
+export default function ToneTeller() {
+  console.log("Tone Teller page");
+  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
 }
