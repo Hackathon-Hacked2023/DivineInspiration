c1147816e58f1c6269adce1808e37ce8dd79a666
goldentoaste <rayg2375@gmail.com>
2023-01-08 08:01:54 -0700

    deleted unsed

--- frontend/tone-teller/src/components/pages/ToneTeller.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function ToneTeller() {
-  console.log("Tone Teller page");
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
-}
