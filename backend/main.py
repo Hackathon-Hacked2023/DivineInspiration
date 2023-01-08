fec1ac635b21a24a8532368951b3fa858ac6cfee
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-01-07 19:40:04 -0700

    Adding files for testing

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,25 @@
-import os
+# py -m flask run
 
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect, request
+from forms import TextForm
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+def home():
+    form = TextForm()
+    return render_template('form.html', title='Register', form = form)
 
 
-if __name__ == "__main__":
+@app.route('/submit/', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        form_data = request.form
+        return '<h1> hello </h1>'
+        #return render_template('results.html', result = form_data['text'])
+
+
+if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
