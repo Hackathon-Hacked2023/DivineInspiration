e463fc6f098c5ac7629a38cdbc0739db42a1604b
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-01-07 20:54:24 -0700

    Clean up
    Deleted the backend directory
    Rewriting files

--- backend/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
-
-class TextForm(FlaskForm):
-    textField = StringField('', validators=[DataRequired(), Length(min=20, max=100)])
-    submit = SubmitField('Submit')
--- backend/readme.txt
+++ backend/readme.txt
@@ -1,1 +1,1 @@
-Flask
+hi
